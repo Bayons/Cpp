2012b4e38bf2c55e01a67935cd73864af7f0a559
Miguel Bayón <mbayons@gmail.com>
2018-11-16 13:36:54 +0100

    Finished 200UVaOJ
    The program reads every words found until it finds the symbol #, generating then the alphabet.
    After that, the program will begin with another alphabet.
    The program can be found here:
    https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=16&page=show_problem&problem=136

--- uDebug/200UVaOJ/alphabet.cpp
+++ uDebug/200UVaOJ/alphabet.cpp
@@ -15,7 +15,8 @@
  * @returns int with the index of the value bounded in [0, n), where
  * 	n is the size of the vector.
  */
-int getIndex(char value, std::vector<char> list){
+int getIndex(char value, std::vector<char> list)
+{
 	int i;
 	for (i = 0; i < (int)list.size(); i++){
 		if(value == list.at(i))
@@ -37,7 +38,8 @@ int getIndex(char value, std::vector<char> list){
  *
  * @returns true if searched is contained in letters.
  */
-bool isInTheList(char searched, std::vector<char> letters){
+bool isInTheList(char searched, std::vector<char> letters)
+{
 	unsigned int i;
 
 	for(i = 0; i < letters.size(); i++){
@@ -53,7 +55,7 @@ bool isInTheList(char searched, std::vector<char> letters){
  * words. Those characters are not repeated.
  *
  * @param words
- * 	vector of string which will be used to generate the
+ *	vector of string which will be used to generate the
  * 	list of characters used.
  *
  * @returns vector of char with every character used.
@@ -74,7 +76,20 @@ std::vector<char> lettersUsed(std::vector<std::string> words)
 	return letters;
 }
 
-int getIndexOfRoot(bool **tree, int size){
+/**
+ * Searches on a graph for a node which doesn't receive any edge and
+ * returns its index on the matrix.
+ *
+ * @param tree
+ * 	bool** which contains the oriented edges between nodes. Here,
+ * 	tree[i][j] == true means that the node i is pointing to node j.
+ * @param size
+ * 	int with the size of the square matrix.
+ *
+ * @returns int with the index of the root node.
+ */
+int getIndexOfRoot(bool **tree, int size)
+{
 	int i, j;
 	for(j = 0; j < size; j++){
 		for (i = 0; i < size; i++){
@@ -86,7 +101,22 @@ int getIndexOfRoot(bool **tree, int size){
 	return -1;
 }
 
-bool isLastNode(bool **tree, int size, int node){
+/**
+ * Checks if a given node is the last node, that is, a node which is
+ * son of another node but is not father of other node.
+ *
+ * @param tree
+ * 	bool** which contains the oriented edges between nodes. Here,
+ * 	tree[i][j] == true means that the node i is pointing to node j.
+ * @param size
+ * 	int with the size of the square matrix.
+ * @param node
+ * 	int with the index of the node which is about to be checked.
+ *
+ * @returns true if it's the last node.
+ */
+bool isLastNode(bool **tree, int size, int node)
+{
 	int i;
 	for(i = 0; i < size; i++){
 		if(tree[node][i])
@@ -95,7 +125,21 @@ bool isLastNode(bool **tree, int size, int node){
 	return true;
 }
 
-bool contains(std::vector<int> parents, int node){
+/**
+ * Checks if the given vector contains a given number. In this context,
+ * this function checks if a node (the number) is an already read father
+ * node (every node in the vector).
+ *
+ * @param parents
+ * 	vector of integers which contains every read parents of the graph.
+ * @param node
+ * 	int which represents the node that may be a father node.
+ *
+ * @returns true if the number is contained in the vector, that is, if the
+ * 	node is an already read father.
+ */
+bool contains(std::vector<int> parents, int node)
+{
 	int i;
 
 	for(i = 0; i < (int)parents.size(); i++){
@@ -105,44 +149,78 @@ bool contains(std::vector<int> parents, int node){
 	return false;
 }
 
+/**
+ * Checks if a node is son of another node and, if the son node has more
+ * parents, checks if the given father is the only non-read father.
+ *
+ * @param tree
+ * 	bool** which contains the oriented edges between nodes. Here,
+ * 	tree[i][j] == true means that the node i is pointing to node j.
+ * @param size
+ * 	int with the size of the square matrix.
+ * @param son
+ * 	int which represents the son node.
+ * @param father
+ *	int which represents the father node.
+ * @param parents
+ *	vector of integers which contains the already read parents.
+ *
+ * @return true	if the father node is pointing to the son node and there
+ * 	isn't any other node out of the vector pointing to this son node.
+ */
 bool isTheOnlyFather(bool **tree, int size, int son, int father,
-	std::vector<int> parents){
+	std::vector<int> parents)
+{
+	bool fatherFound = false;
 	int i;
 
 	for(i = 0; i < size; i++){
-		//if this father is not the given father
-		//and
-		//this father is not in the list
-		//return false
-		if(tree[i][son] && i != father && !contains(parents, i)){
-			return false;
+		if(tree[i][son]){
+			if(i == father)
+				fatherFound = true;
+			else if(!contains(parents, i)){
+				return false;
+			}
 		}
 	}
-	return true;
+	return fatherFound;
 }
 
+/**
+ * Receives a boolean matrix with the links between nodes and prints the
+ * sorted list of every element contained in the graph. The algorithm is
+ * described this way:
+ * Rules:
+ *  + Each node must be (1,1) at the end, except
+ *    for node n0 which must be (0,1) and node nN, which
+ *    must be (1,0)
+ *  + Once a node is read, it's added to the list of
+ *    read elements R
+ *
+ * Process:
+ *  + Searches n0 by searching for a (0,1) node.
+ *  + nX = n0
+ *  + Loop:
+ *  	+ Searches for a son node nX+1 of nX
+ *  	   whose only father out of R is nX
+ * 	+ Stores nX in R
+ *	+ If(nX+1 didn't appear)
+ *		+ Congrats! It is nN
+ *		+ End of the sorting
+ *	+ nX+1 now is nX
+ *	+ R contains the list
+ *
+ * @param tree
+ * 	bool** which contains the oriented edges between nodes. Here,
+ * 	tree[i][j] == true means that the node i is pointing to node j.
+ * @param size
+ * 	int with the size of the square matrix.
+ * @param letters
+ * 	vector of characters which maps every character used with their
+ * 	index.
+ */
 void sortTree(bool **tree, int size, std::vector<char> letters)
 {
-/*	Rules:
- *	+ Each node must be (1,1) at the end, except
- *	for node n0 which must be (0,1) and node nN, which
- *	must be (1,0)
- *	+ Once a node is read, it's added to the list of
- *	   read elements R
- *
- *	Process:
- *		+ Searches n0 by searching for a (0,1) node.
- *		+ nX = n0
- *		+ Loop:
- *			+ Searches for a son node nX+1 of nX
- *			   whose only father out of R is nX
- *			+ Stores nX in R
- *			+ If(nX+1 didn't appear)
- *				+ Congrats! It is nN
- *				+ End of the sorting
- *			+ nX+1 now is nX
- *		+ R contains the list
- */
 	int i;
 	int sonNode = -1;
 	int fatherNode = getIndexOfRoot(tree, size);
@@ -165,24 +243,39 @@ void sortTree(bool **tree, int size, std::vector<char> letters)
 	std::cout << '\n';
 }
 
-
-void fillBoolTree(bool **tree, int size, std::vector<std::string> words,
-		std::vector<char> letters)
+/**
+ * Processes a list of sorted words by an unknown alphabet and fills a
+ * graph with oriented edges between nodes. The algorithm follows this
+ * scheme:
+ *
+ * 1: write down links between nodes
+ * 2: search groups of words with the same initial char
+ *   2.1: char to temp; boolean repeated = false
+ *   2.2: if next char is the same, temp to list L and
+ *         repeated = true
+ *   2.3: when next char is different, repeat from 2.1
+ *   2.4: when list is finished, take chars from L
+ *      2.4.1: takes char C
+ *      2.4.2: looks and inserts every word which begins
+ *              with C in list tempList
+ *      2.4.3: removes the first char of every word
+ *      2.4.4: sends tempList to fillBoolTree
+ *
+ * @param tree
+ * 	bool** which contains the oriented edges between nodes. Here,
+ * 	tree[i][j] == true means that the node i is pointing to node j.
+ * @param size
+ * 	int with the size of the square matrix.
+ * @param words
+ * 	vector of strings which contains every word sorted by the same
+ * 	alphabet.
+ * @param letters
+ * 	vector of characters used by the alphabet (bad naming, but late
+ * 	for change it :S)
+ */
+void fillBoolTree(bool **tree, int size, std::vector<std::string> words, 
+	std::vector<char> letters)
 {
-	/*
-	 * 1: write down links between nodes
-	 * 2: search groups of words with the same initial char
-	 *   2.1: char to temp; boolean repeated = false
-	 *   2.2: if next char is the same, temp to list L and
-	 *         repeated = true
-	 *   2.3: when next char is different, repeat from 2.1
-	 *   2.4: when list is finished, take chars from L
-	 *      2.4.1: takes char C
-	 *      2.4.2: looks and inserts every word which begins
-	 *              with C in list tempList
-	 *      2.4.3: removes the first char of every word
-	 *      2.4.4: sends tempList to fillBoolTree
-	 */
 	unsigned int i, j, indexA, indexB;
 	char tempChar = '\0';
 	std::vector<char> groups;
@@ -195,6 +288,7 @@ void fillBoolTree(bool **tree, int size, std::vector<std::string> words,
 		indexB = getIndex(words.at(i).at(0), letters);
 		if(indexA != indexB)
 			tree[indexA][indexB] = true;
+		std::cout << indexA << "-" << indexB << '\n';
 	}
 
 	// Checks for groups of words and writes down the initial characters
@@ -202,7 +296,8 @@ void fillBoolTree(bool **tree, int size, std::vector<std::string> words,
 		if(words.at(i).at(0) != tempChar){
 			tempChar = words.at(i).at(0);
 			repeated = false;
-		} else if(words.at(i).at(0) == tempChar && repeated == false){
+		} else if(words.at(i).at(0) == tempChar &&
+			       	repeated == false){
 			groups.push_back(tempChar);
 			repeated = true;
 		}
@@ -216,7 +311,8 @@ void fillBoolTree(bool **tree, int size, std::vector<std::string> words,
 				words.at(j).substr(1) != ""){
 				tempList.push_back(words.at(j));
 				tempList.at(tempList.size()-1).
-					erase(tempList.at(tempList.size()-1).begin());
+					erase(tempList.at(tempList.
+					size()-1).begin());
 			}
 		}
 
@@ -226,33 +322,61 @@ void fillBoolTree(bool **tree, int size, std::vector<std::string> words,
 
 }
 
-int main(void)
-{
-	std::vector<std::string> words;
+/**
+ * Receives a set of words and generates the alphabet which sorts the
+ * received set. It is sorted in two parts:
+ * 1. generation of a graph.
+ * 2. elimination of unnecessary edges between nodes.
+ * At the end of the simplification of the graph, the function will print
+ * the alphabet.
+ *
+ * @param words
+ * 	vector of strings which contains the set of words and is the base
+ * 	of the alphabet which will be generated.
+ */
+void alphabet(std::vector<std::string> words){
 	std::string temp;
 	unsigned int size, i, j;
-
-	while (std::cin >> temp && temp != END)
-	{
-			words.push_back(temp);
-	}
-
 	std::vector<char> letters(lettersUsed(words));
 	size = letters.size();
-
 	bool **tree = new bool*[size];
-	for(i = 0; i < size; i++) // How to find array size
-	{
+	
+	for(i = 0; i < size; i++){
 		tree[i] = new bool[size];
-		for(j = 0; j < size; j++)
-		{
+		for(j = 0; j < size; j++){
 			tree[i][j] = false;
 		}
 	}
 
 	fillBoolTree(tree, size, words, letters);
 
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			std::cout << tree[i][j];
+		}
+		std::cout << "\n";
+	}
+
 	sortTree(tree, size, letters);
+}
+
+/**
+ * Reads every word given as an input and generates the alphabet that
+ * sorts those words when the symbol # is found.
+ */
+int main(void)
+{
+	std::vector<std::string> words;
+	std::string temp;
+
+	while (std::cin >> temp)
+	{
+			if (temp == END) {
+				alphabet(words);
+				words.clear();
+			} else
+			words.push_back(temp);
+	}
 
 	return 0;
 }
--- uDebug/200UVaOJ/run.sh
+++ uDebug/200UVaOJ/run.sh
@@ -1,2 +1,4 @@
-make
-./iniAlphabet < in/ejemplo
+./iniAlphabet < in/ejemplo > out/ejemplo.out
+./iniAlphabet < in/1 > out/1.out
+./iniAlphabet < in/2 > out/2.out
+./iniAlphabet < in/3 > out/3.out
