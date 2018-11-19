18e9a7ba3973194268b473e316a5ebc581de571a
Miguel Bayón <mbayons@gmail.com>
2018-11-19 11:56:20 +0100

    Tidied CCC foulder

--- CCC18-11/1/run.sh
+++ CCC18-11/1/run.sh
@@ -1,5 +1,6 @@
 make
-./exeBuildings < 1/level1_0.in > 1/0.out
-./exeBuildings < 1/level1_1.in > 1/1.out
-./exeBuildings < 1/level1_2.in > 1/2.out
-./exeBuildings < 1/level1_3.in > 1/3.out
+./exeBuildings < ejemplo.in
+./exeBuildings < level1_0.in > 0.out
+./exeBuildings < level1_1.in > 1.out
+./exeBuildings < level1_2.in > 2.out
+./exeBuildings < level1_3.in > 3.out
---
+++ CCC18-11/2/run.sh
@@ -0,0 +1,6 @@
+make
+./exeBuildings < in/example.in
+./exeBuildings < in/level2_0.in > out/0.out
+./exeBuildings < in/level2_1.in > out/1.out
+./exeBuildings < in/level2_2.in > out/2.out
+./exeBuildings < in/level2_3.in > out/3.out
